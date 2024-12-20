9af0cff4d814e453a3c74e7398f292413582334f
MEREIAA <mereiaalmazbekowa@gmail.com>
2024-12-20 18:13:52 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw345/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.hw345;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity implements InputFragment.OnMessageSendListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.chatFragmentContainer, new ChatFragment())
+                .commit();
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.inputFragmentContainer, new InputFragment())
+                .commit();
+    }
+
+    @Override
+    public void onMessageSend(String message) {
+        ChatFragment chatFragment = (ChatFragment) getSupportFragmentManager().findFragmentById(R.id.chatFragmentContainer);
+        if (chatFragment != null) {
+            chatFragment.addUserMessage(message);
+        }
+    }
+}
